14ca3548923c7bc2b41623478fb5fa18817f129f
makrem89 <Makrem.Zitoun@wevioo.com>
2025-12-31 19:52:42 +0100

    upload

--- olivision-backend/src/controller/analysisController.js
+++ olivision-backend/src/controller/analysisController.js
@@ -6,7 +6,7 @@ const baseUrl = process.env.BASE_URL;
 
 const upload = async (req, res) => {
   try {
-    // await uploadFile(req, res);
+    await uploadFile(req, res);
 
     if (req.file == undefined) {
       return res.status(400).send({ message: "Please upload a file!" });
